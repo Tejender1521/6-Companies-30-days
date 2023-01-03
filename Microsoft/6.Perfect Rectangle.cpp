315b6ca47550e7cb8ecd454b8dc0752fb374c014
Tejender Upadhyay <2020kucp1019@iiitkota.ac.in>
2023-01-03 21:11:35 +0530

    Seven Question Solved

---
+++ Microsoft/6.Perfect Rectangle.cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+public:
+    bool isRectangleCover(vector<vector<int>> &rectangles)
+    {
+        int min_x = INT_MAX, min_y = INT_MAX;
+        int max_x = INT_MIN, max_y = INT_MIN;
+
+        int total_area = 0;
+
+        for (const auto &rectangle : rectangles)
+        {
+            min_x = min(min_x, rectangle[0]);
+            min_y = min(min_y, rectangle[1]);
+            max_x = max(max_x, rectangle[2]);
+            max_y = max(max_y, rectangle[3]);
+
+            total_area += (rectangle[2] - rectangle[0]) * (rectangle[3] - rectangle[1]);
+        }
+
+        int width = max_x - min_x;
+        int height = max_y - min_y;
+        int region_area = width * height;
+
+        return total_area == region_area;
+    }
+};
