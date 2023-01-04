beca44848ff30ac2f108d870c819a6ba92fde018
Tejender Upadhyay <2020kucp1019@iiitkota.ac.in>
2023-01-04 14:38:05 +0530

    13 question solved

---
+++ Microsoft/13.Airplane Seat Arrangement Probability.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    double nthPersonGetsNthSeat(int n)
+    {
+        if (n == 1)
+            return 1 / double(n);
+        else
+            return 1 / (2.00000);
+    }
+};
