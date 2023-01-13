85ceec8e9bc55bb3eb796de6b9823ffc531c1255
Tejender Upadhyay <2020kucp1019@iiitkota.ac.in>
2023-01-13 13:40:52 +0530

    12 question solved

---
+++ Adobe/12.Shortest unsorted continuous subarray.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+class Solution
+{
+public:
+    int findUnsortedSubarray(vector<int> &nums)
+    {
+        vector<int> temp(nums);
+        sort(temp.begin(), temp.end());
+        int i = 0, j = nums.size();
+        if (temp == nums)
+        {
+            return 0;
+        }
+        for (int itr = 0; itr < nums.size(); itr++)
+        {
+            if (nums[itr] != temp[itr])
+            {
+                i = itr;
+                break;
+            }
+        }
+        for (int itr = nums.size() - 1; itr >= 0; itr--)
+        {
+            if (nums[itr] != temp[itr])
+            {
+                j = itr;
+                break;
+            }
+        }
+        return j - i + 1;
+    }
+};
